132b743f1714387ba8b973ad5148c0bbaa794e98
SudarshanGp <Sudarshan@Sudarshans-MacBook-Air.local>
2015-04-24 15:15:44 -0500

    updated js
    js

--- Extensino/src/browser_action/browser_action.js
+++ Extensino/src/browser_action/browser_action.js
@@ -8,14 +8,36 @@ var num_clicks_settings = 0;
 var today_data = {};
 var tomorrow_data = {};
 var yesterday_data = {};
+
 $(document).ready(function(){
 
-$('#setting_frame').on('click', function(event) {
-		//alert("clicked");
-		num_clicks_settings = num_clicks_settings +1;
- 	//	$('#setting_frame').html('window.html');
- 	// $('<iframe id="myIframe" src="window.html" width="200" height="200"></iframe>');
-     	if(num_clicks_settings== 1)
+	$('#setting_frame').on('click', function(event) {
+		control_setting();
+	    });
+
+	$('#yesterday').on('click', function(event)
+		{
+			generate_yesterday();
+		});
+	$('#tomorrow').on('click', function(event)
+	{
+			generate_tomorrow();
+	});
+
+	setupPage();
+	generate_sidebar();
+});
+
+function generate_sidebar()
+{
+
+
+}
+
+function control_setting()
+{
+	num_clicks_settings = num_clicks_settings +1;
+    if(num_clicks_settings== 1)
      	{
 			iframe = document.createElement("IFRAME");
   			iframe.setAttribute("src", "window.html");
@@ -36,20 +58,18 @@ $('#setting_frame').on('click', function(event) {
         {
         	$(iframe).hide();
         }
+}
 
-     	
-         
-    });
+function generate_tomorrow()
+{
 
+}
 
+function generate_yesterday()
+{	
+	//alert("Yest");
+}
 
-	setupPage();
-});
-// $('#button').click(function() {
-//    // if($('#myIframe').size() == 0) {
-//    //     $('<iframe id="myIframe" src="window.html" width="200" height="200"></iframe>');
-//    // }
-// });
 
 
 function setupPage()
@@ -59,29 +79,20 @@ function setupPage()
  		var data = snapshot.val();
  		today_data = data;
  		renderToday();
- 	//	alert(1);
- 	// 	 var bkg = chrome.extension.getBackgroundPage();
-		// bkg.console.log(data);
 		},	 function (errorObject) {
   		console.log("The read failed: " + errorObject.code);
 		});
  	yesterday.on("value", function(snapshot) {
- 		// console.log(snapshot.val());
  		var data1 = snapshot.val();
  		yesterday_data = data1;
- 	//	renderYesterday();
- 	// 	 var bkg = chrome.extension.getBackgroundPage();
-		// bkg.console.log(data);
+ 		renderYesterday();
 		},	 function (errorObject) {
   		console.log("The read failed: " + errorObject.code);
 		});
  	tomorrow.on("value", function(snapshot) {
- 		// console.log(snapshot.val());
  		var data2 = snapshot.val();
  		tomorrow_data = data2;
- 	//	renderTomorrow();
- 	// 	 var bkg = chrome.extension.getBackgroundPage();
-		// bkg.console.log(data);
+ 		renderTomorrow();
 		},	 function (errorObject) {
   		console.log("The read failed: " + errorObject.code);
 		});
@@ -90,10 +101,26 @@ function setupPage()
 
  function renderToday()
  {
- 	//alert("I'm here");
- 	$("#test").text("tester");
+ 	var parse_date = today_data['date'].split("-");
+ 	var returner = parse_date[1]+"/"+parse_date[2];
+ 	$("#today").text('today');
 
+
+ 	//today
  }
+function renderYesterday()
+{
+	var parse_date = yesterday_data['date'].split("-");
+ 	var returner = parse_date[1]+"/"+parse_date[2];
+	$("#yesterday").text(returner);
+
+}
+function renderTomorrow()
+{
+	var parse_date= tomorrow_data['date'].split("-");
+	var returner = parse_date[1]+"/"+parse_date[2];
+	$("#tomorrow").text(  returner);
 
 
+}
 
