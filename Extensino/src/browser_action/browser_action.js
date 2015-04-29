7d93c7ee047fece561972cfc196d7978bd337d5e
mgonnella41 <mgonnella41@gmail.com>
2015-04-28 19:14:13 -0500

    Last Play is loading, but not Indv. Players

--- Extensino/src/browser_action/browser_action.js
+++ Extensino/src/browser_action/browser_action.js
@@ -244,8 +244,8 @@ function generate_yesterday_game2() // WHY IS IT NOT WORKING...DEBUG.
 	$("#f_2").text(yesterday_data['games']['2']['Team_stats']['home']['points']);
 	/*generate play-by-play*/
 	//TODO: Add the quarter to the time so it's more clear
-	$("#p1_t").text(yesterday_data['games']['2']['Play_by_play']['periods']['3']['events']['98']['clock']);
-	$("#p1").text(yesterday_data['games']['2']['Play_by_play']['periods']['3']['events']['98']['description']);
+	$("#p1_t").text(yesterday_data['games']['2']['Play_by_play']['periods']['3']['events']['75']['clock']);
+	$("#p1").text(yesterday_data['games']['2']['Play_by_play']['periods']['3']['events']['75']['description']);
 	/*generate team stats (can't find largest lead)*/
 	$("#reb_1").text(yesterday_data['games']['2']['Team_stats']['away']['statistics']['rebounds']);
 	$("#reb_2").text(yesterday_data['games']['2']['Team_stats']['home']['statistics']['rebounds']);
